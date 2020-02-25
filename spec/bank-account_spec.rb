5b439a5a458dbb0585d62e04ded785fe8bcddba8
Ilias Grigoropoulos <ilias_grigorop2@hotmail.com>
2020-02-25 11:09:15 +0000

    Add tests for balance method

--- spec/bank-account_spec.rb
+++ spec/bank-account_spec.rb
@@ -2,10 +2,36 @@ require 'bank-account.rb'
 describe BankAccount do
   context 'When balance is zero' do
     describe "#balance" do
-      it "returns zero" do
+      it 'returns zero' do
        account = BankAccount.new
        expect(account.balance).to eq(0)
       end
+    end
+  end
+  context 'When deposit is made' do
+    describe '#deposit' do
+      context 'When a deposit of 1000 is made' do
+        it 'Returns 1000' do
+          account = BankAccount.new
+          expect(account.deposit(1000)).to eq(1000)
+        end
+      end
+      context 'When a deposit of 500 is made' do
+        it 'Returns 500' do
+          account = BankAccount.new
+          expect(account.deposit(500)).to eq(500)
+        end
+      end
+    end
+    describe '#balance' do
+      context 'When a deposit of 1000 and a deposit of 700 are made' do
+        it 'Returns balance of 1700' do
+          account = BankAccount.new
+          account.deposit(1000)
+          account.deposit(700)
+          expect(account.balance).to eq(1700)
+        end
+      end 
     end
   end
 end
