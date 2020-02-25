434bf44f52d7405596e039d5cd73f4fb34fd5965
Ilias Grigoropoulos <ilias_grigorop2@hotmail.com>
2020-02-25 13:18:20 +0000

    Rename deposit method name to deposit_money

---
+++ lib/bank_account.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# class definition
+class BankAccount
+  attr_reader :balance
+  def initialize
+    @balance = 0
+  end
+
+  def deposit_money(money)
+    # returns the balance
+    @balance += money
+  end
+
+  def withdraw_money(money)
+    # returns the balance
+    @balance -= money
+  end
+end
